04fd7649b696649f98fa6a161703722882f93c25
programmingwithsoumyansh <programmingwithsoumyansh@gmail.com>
2024-12-17 20:23:53 +0530

    Added new tutorial

---
+++ Tutorial 3 - Variables And Data Types/VariablesAndDataTypes/src/Sample.java
@@ -0,0 +1,17 @@
+public class Sample {
+
+    public static void main(String[] args) {
+
+        int i = 5;  // numbers
+        String s = "Hello World";
+        char letter = 'a';
+        double d = 5.99;
+        boolean isAgeGreaterThanEighteen = true;
+        // Printing Variables
+        System.out.println(i);
+        System.out.println(s);
+        System.out.println(letter);
+        System.out.println("My integer value is:" + i);
+        System.out.println("Is Age Greater Than 18:"+isAgeGreaterThanEighteen);
+    }
+}
